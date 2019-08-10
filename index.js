4d1860ca4d0a96009e8f49ffccec1fc5ed93cc92
Dominic <dominic.m.chu@gmail.com>
2019-08-10 05:07:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -61,7 +61,7 @@ function placeOrder(cardNumber) {
   }
   else {
     cart = []
-    return ("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+    return ("Your total cost is $" + totalPrice + ", which will be charged to the card " + cardNumber + ".")
   }
 
 }
