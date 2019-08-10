88cb9485763fbccf0a5f0daae6a110fff1841f7c
Dominic <dominic.m.chu@gmail.com>
2019-08-10 05:12:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,10 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }
   else {
-    return ("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".")
+    return ("Your total cost is $" + total() + ", which will be charged to the card " + cardNumber + ".");
+    for (var i = 0; i < cart.length; i++) {
+      cart.pop();
+    }
   }
 
 }
