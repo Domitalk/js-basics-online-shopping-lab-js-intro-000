805e58e5f5583197b1b082e4ac978df076039747
Dominic <dominic.m.chu@gmail.com>
2019-08-10 05:00:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,8 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if(cardNumber < 1) {
+  if (cardNumber === undefined) {
     return `Sorry, we don't have a credit card on file for you.`
   }
+  
 }
